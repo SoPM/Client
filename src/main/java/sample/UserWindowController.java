c8db4a56a9004139acf631bf4d4948bbe33a532f
Franciscus <31704859+VasyaXXI@users.noreply.github.com>
2019-12-18 01:45:06 +0300

    Add files via upload

---
+++ src/main/java/sample/UserWindowController.java
@@ -0,0 +1,38 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+
+public class UserWindowController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableView<?> projects;
+
+    @FXML
+    private Tab func1;
+
+    @FXML
+    private TableView<?> table_for_jenya;
+
+    @FXML
+    private Button enter_button;
+
+    @FXML
+    private Tab func2;
+
+    @FXML
+    private TableView<?> table_for_trello;
+
+    @FXML
+    void initialize() {
+
+    }
+}
